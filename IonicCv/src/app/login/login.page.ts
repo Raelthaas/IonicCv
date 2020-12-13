4ceae4e105765e37c94029dbaa953c9cd3489f1e
Tom Robert <tom.robert@epitech.eu>
2020-12-13 10:34:16 +0100

    add full project

---
+++ IonicCv/src/app/login/login.page.ts
@@ -0,0 +1,39 @@
+import { Component,OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthenticationService } from "../shared/authetication.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+
+export class LoginPage implements OnInit {
+
+  constructor(
+    public authService: AuthenticationService,
+    public router: Router
+  ) {}
+
+  ngOnInit() {}
+
+  logIn(email, password) {
+    this.authService.SignIn(email.value, password.value)
+      .then((res) => {
+        if(this.authService.isEmailVerified) {
+          this.router.navigate(['/tabs'])
+        } else {
+          window.alert('wrong email or password')
+          return false;
+        }
+      }).catch((error) => {
+        window.alert(error.message)
+      })
+  }
+
+  Register() {
+    this.router.navigate(['/register'])
+
+  }
+
+}
---
+++ IonicCv/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDgS65kF5M8ZeQ6m3c8Nbej5777Sxsig6k",
+    authDomain: "ioniccv-9771c.firebaseapp.com",
+    projectId: "ioniccv-9771c",
+    storageBucket: "ioniccv-9771c.appspot.com",
+    messagingSenderId: "765386926150",
+    appId: "1:765386926150:web:b916da62e786df95b1c247",
+    measurementId: "G-4G82G0WK1F"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
